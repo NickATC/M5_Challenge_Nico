de37d6f4634f6d2873c5b894625683ae017f8e7c
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2022-02-24 09:19:38 -0500

    Initial Commit

---
+++ M5_WrapUpChallenge_Nico/M5_WrapUpChallenge_NicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5_WrapUpChallenge_NicoApp.swift
+//  M5_WrapUpChallenge_Nico
+//
+//  Created by NICOLAS  TAUTIVA on 24/02/22.
+//
+
+import SwiftUI
+
+@main
+struct M5_WrapUpChallenge_NicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
